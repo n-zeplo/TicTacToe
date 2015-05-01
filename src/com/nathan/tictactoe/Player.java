6783901fac40058db7df8311ba0c2eb6ae580435
nzeplo <nzeplowitz@gmail.com>
2015-04-30 20:56:40 -0500

    Encapsulate Return User Input to Player

--- src/com/nathan/tictactoe/Player.java
+++ src/com/nathan/tictactoe/Player.java
@@ -1,15 +1,30 @@
 package com.nathan.tictactoe;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  * Created by nzeplowi on 4/30/15.
  */
 public class Player {
     public String name;
     public String piece;
+    private BufferedReader bufferedReader;
 
-    public Player(String name, String piece) {
+    public Player(String name, String piece, BufferedReader bufferedReader) {
         this.name = name;
         this.piece = piece;
+        this.bufferedReader = bufferedReader;
     }
 
+
+    public String returnUserInput() {
+        String input = "";
+        try {
+            input = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
 }
